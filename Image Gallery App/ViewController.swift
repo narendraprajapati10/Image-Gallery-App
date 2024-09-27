e3705e6b9049c7d61c41cc9b029e28295ae7fe92
narendraprajapati10 <narendraprajapati10@gmail.com>
2024-09-27 10:15:02 +0530

    Initial Commit

---
+++ Image Gallery App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Gallery App
+//
+//  Created by WhyQ on 27/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Image Gallery AppUITests/Image_Gallery_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Image_Gallery_AppUITestsLaunchTests.swift
+//  Image Gallery AppUITests
+//
+//  Created by WhyQ on 27/09/24.
+//
+
+import XCTest
+
+final class Image_Gallery_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
